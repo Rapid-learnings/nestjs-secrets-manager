27b8aab3994dfe17564d31ae3af7b558217d6444
Tapas Thakkar <tapasthakkar@gmail.com>
2022-05-11 18:09:47 +0530

    fix: fetching and loading secret from secret manager

--- src/config/aws-config.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AwsConfigService } from './aws-config.service';
-
-@Module({
-  providers: [AwsConfigService],
-  exports: [AwsConfigService]
-})
-export class AwsConfigModule {}
